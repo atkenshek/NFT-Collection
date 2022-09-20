2d478c2354651aba7b0ec110f4a797846e39a3e4
Meiram Sopy Temirzhanov <random9804@yandex.ru>
2022-09-21 00:13:33 +0600

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xaEe340bAda9802E7C7324F583b5a2fbA623DB196";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
